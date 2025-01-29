edc9956578b785e4b0fe04e63123bd06f56da66d
tannmayygupta <guptatb_1@rknec.edu>
2025-01-29 23:59:53 +0530

    Codechef weekly contest 29th jan

---
+++ SquidGame.java
@@ -0,0 +1,26 @@
+// contest of codechef  29/01/2027
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int N ;
+		int m = 0 ;
+		int K ;
+		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		
+		m = N - K ;
+		
+		System.out.println(m*10000);
+		
+
+	}
+}
