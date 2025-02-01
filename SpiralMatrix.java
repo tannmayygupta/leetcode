80a506c29a2aa250cd8918a121e323187801aa1d
tannmayygupta <guptatb_1@rknec.edu>
2025-02-01 19:36:30 +0530

    Completed Qno.54 leetcode Spiral Matrix

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,8 +1,51 @@
+import  java.util.* ;
+
 public class SpiralMatrix {
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-        int n = arr.length ; // row 
-        int m = arr[0].length ; // colo
-        int[][] spiral = new int[n][m];
+        List<Integer> spiral = new ArrayList<Integer>();
+        // int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+        int m = arr.length ; // row 
+        int n = arr[0].length ; // colo
+
+        int i = 0 ;
+        int j = 0 ;
+
+        while(j < n){
+            spiral.add(arr[i][j]);
+            j++;
+        }
+
+        j = j - 1 ;
+
+
+        while(i < m - 1 ){
+            i++;
+            spiral.add(arr[i][j]);
+        }
+
+
+        // System.out.println(i); // i == 2
+
+        // System.out.println(j-1);
+
+        while(j - 1 >= 0 ){
+            j--;
+            spiral.add(arr[i][j]);
+        }
+
+        System.out.println(j); // 0
+
+        i = i - 1 ;
+
+        System.out.println(i);
+
+        while(j < n - 1){
+            spiral.add(arr[i][j]);
+            j++;
+        }
+
+        System.out.println(spiral);
     }
 }
