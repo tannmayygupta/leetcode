85c8a4b7652148412098e4c2168adec1a01a1771
tannmayygupta <guptatb_1@rknec.edu>
2025-02-10 21:59:03 +0530

    Daily leetcode 3174 Clear digit completed

--- Cleardigit.java
+++ Cleardigit.java
@@ -25,7 +25,7 @@ public class Cleardigit {
             //     System.out.println(arr[i]);
             // }
             StringBuilder sb = new StringBuilder();
-            sb.append(arr);
+            sb.append(arr, 0, tos + 1); // Append only valid characters
             String str = sb.toString();
             System.out.println(str);
             // return str ;
@@ -53,3 +53,68 @@ public class Cleardigit {
         return tos ;
     }
 }
+
+
+
+
+
+
+/*class Solution {
+    public String clearDigits(String s) {
+    //     public class Cleardigit {
+    // public static void main(String[] args) {
+
+        // String s = "cb34";
+        int n = s.length();
+        char[] arr = new char[n];
+
+        int tos = -1 ;
+
+        for(int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i) ;
+            if(Character.isDigit(ch)){
+                tos = pop(tos , arr);
+            }
+            else{
+                tos = push(ch , tos , arr);
+            }
+        }
+
+        if(tos == -1){
+            // System.out.println("");
+            return "" ;
+        }
+        else{
+            // for(int i = 0 ; i <= tos ; i++){
+                // System.out.println(arr[i])''
+            // }
+            StringBuilder sb = new StringBuilder();
+                sb.append(arr, 0, tos + 1); // Append valid char
+                String str = sb.toString();
+            return str ;
+        }
+    }
+
+    static int pop(int tos , char[] arr) {
+        if (tos == -1) {
+            return -1 ;
+        } 
+        else {
+            return tos - 1;
+        }
+    }
+
+    static int push(char value , int tos , char[] arr) {
+        int n =  arr.length ;
+        if (tos == n - 1) {
+            return tos ;
+        } 
+        else{
+            tos++;
+            arr[tos] = value;
+        }
+        return tos ;
+    }
+// }
+
+} */
