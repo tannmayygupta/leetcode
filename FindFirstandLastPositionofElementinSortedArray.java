cd9876dddf79bdb13f120a08544b35d5e284bdd7
tannmayygupta <guptatb_1@rknec.edu>
2025-03-04 23:35:37 +0530

    34. Find First and Last Position of Element in Sorted Array(was getting MLE when used Binary search once but then used binary search twice so that first index of target (right = mid +1 ) and last index of target(left = mid - 1) this use of two times binary search helps to find first and last paosition of element)

---
+++ FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindFirstandLastPositionofElementinSortedArray {
+    public static void main(String[] args) {
+
+        int arr[] = {5,7,7,8,8,10};
+
+        int target = 8 ;
+
+        int arr2[] = searchRange(arr, target);         
+
+        for(int i = 0 ; i < arr2.length ; i++){
+            System.out.println(arr2[i]);
+        }
+    }
+        public static int[] searchRange(int[] arr, int target) {
+            int n = arr.length - 1;
+            int left = 0;
+            int right = n - 1;
+            int index = 0;
+            int arr1[] = {-1,-1};
+    
+            while (left <= right) {
+    
+                int mid = (left + right) / 2;
+    
+                if (arr[mid] == target) {
+                    arr1[0]= mid ;
+                    right = mid - 1;
+                } else if (arr[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+    
+            left = 0;
+            right = n - 1;
+    
+            while (left <= right) {
+                int mid = (left + right) / 2;
+    
+                if (arr[mid] == target) {
+                    arr1[1]= mid ;
+                    left = mid + 1;
+                } else if (arr[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+    
+        return arr1 ;
+    }
+}
