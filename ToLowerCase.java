5a3d414256df5b566399950e062e37b7ddb7104c
tannmayygupta <guptatb_1@rknec.edu>
2025-03-30 01:18:26 +0530

    709. To Lower Case

---
+++ ToLowerCase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = new String();
+        s.toLowerCase();
+        System.out.println(s);
+    }    
+}
