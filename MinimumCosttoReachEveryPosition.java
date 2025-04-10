29be04dacd18b0afc180b51cb875e61c3e40ef99
tannmayygupta <guptatb_1@rknec.edu>
2025-04-10 22:10:16 +0530

    3502. Minimum Cost to Reach Every Position

---
+++ MinimumCosttoReachEveryPosition.java
@@ -0,0 +1,24 @@
+public class MinimumCosttoReachEveryPosition {
+    public static void main(String[] args) {
+        int[] cost = [5,3,3,1,4,2];
+        int[] a = minCosts(cost);
+        
+    }
+    public static int[] minCosts(int[] cost) {
+    
+            int[] arr = new int[cost.length];
+    
+            int min = cost[0] ;
+    
+            for(int i = 0 ; i < cost.length ;i++){
+    
+                if(cost[i] < min){
+                    min = cost[i];
+                }
+                arr[i] = min ;
+            }
+    
+            return arr ;
+            
+    }
+}
