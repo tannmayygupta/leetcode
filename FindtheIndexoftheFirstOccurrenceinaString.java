593c2e98dfbb63ea0db57285f69fc1632820b4a8
tannmayygupta <guptatb_1@rknec.edu>
2025-05-27 18:22:29 +0530

    28. Find the Index of the First Occurrence in a String(optimized approach of mine)

--- FindtheIndexoftheFirstOccurrenceinaString.java
+++ FindtheIndexoftheFirstOccurrenceinaString.java
@@ -6,36 +6,59 @@ public class FindtheIndexoftheFirstOccurrenceinaString {
         String hayStack = sc.nextLine();
         String needle = sc.nextLine();
 
-        int x = strStr(hayStack, needle);
-
-        System.out.println(x);
+        System.out.println(strStr(hayStack, needle));
 
         
     }
 
+    // public static int strStr(String haystack, String needle) {
+    
+    //         if(needle.isEmpty()) {
+    //             return 0;
+    //         }
+    //         else if(haystack.length() < needle.length()) {
+    //             return -1;
+    //         }
+    //         else if(haystack.length() == needle.length() && haystack.equals(needle)) {
+    //             return 0;
+    //         } 
+    //         else {
+    //             for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+    //                 String s = "";
+    //                 for (int j = 0; j < needle.length(); j++) {
+    //                     s += haystack.charAt(i + j);
+    //                 }
+    //                 if (s.equals(needle)) {
+    //                     return i;
+    //                 }
+    //             }
+    //         }
+    
+    //         return -1;
+    //     }
+
+
     public static int strStr(String haystack, String needle) {
-    
-            if(needle.isEmpty()) {
-                return 0;
-            }
-            else if(haystack.length() < needle.length()) {
-                return -1;
-            }
-            else if(haystack.length() == needle.length() && haystack.equals(needle)) {
-                return 0;
-            } 
-            else {
-                for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-                    String s = "";
-                    for (int j = 0; j < needle.length(); j++) {
-                        s += haystack.charAt(i + j);
-                    }
-                    if (s.equals(needle)) {
-                        return i;
-                    }
-                }
-            }
-    
+
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        if(haystack.length() < needle.length()){
             return -1;
         }
+
+        for(int i = 0 ; i <= haystack.length() - needle.length() ; i++){
+            int st = i ; 
+            int end = i + needle.length() ;
+
+            if(haystack.substring(st, end).equals(needle)){
+                return st ;
+            } 
+
+        }
+
+        return -1;
+}
+
 }
