ce2a79fc4b9db191e858d58f2cd414894cda8a48
tannmayygupta <guptatb_1@rknec.edu>
2025-06-01 17:00:16 +0530

    131. Decompress Run-Length Encoded List

--- DecompressRunLengthEncodedList.java
+++ DecompressRunLengthEncodedList.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class DecompressRunLengthEncodedList{
     public static void main(String[] args) {
 
-        int[] nums = [1,2,3,4];
+        int[] nums = {1,2,3,4};
 
         int[] a = decompressRLElist(nums);
 
