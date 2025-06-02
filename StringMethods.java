fd02e84e7c2e56abf267942c3d3f3b72c0f31bd0
tannmayygupta <guptatb_1@rknec.edu>
2025-06-03 00:09:02 +0530

    learnig String methods for que no. 151 lc

---
+++ StringMethods.java
@@ -0,0 +1,18 @@
+// learing the string methods in java 
+
+public class StringMethods{
+    public static void main(String[] args) {
+        String s = "                                  Tanmaayy     Samiksha   Devi ji  " ;
+        // trim method helps to delete the extra spaces form start and end 
+        System.out.println(s.trim());
+
+
+        // split method helps to create an array of strings that help us to 
+
+        String[] ans = s.split("\\s\\.\\");
+        
+        
+        
+
+    }
+}
