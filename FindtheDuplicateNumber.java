895efe1db737c85d826d8b8b835fda793deab220
tannmayygupta <guptatb_1@rknec.edu>
2025-06-06 21:59:43 +0530

    287. Find the Duplicate Number

--- FindtheDuplicateNumber.java
+++ FindtheDuplicateNumber.java
@@ -1,7 +1,7 @@
 public class FindtheDuplicateNumber {
     public static void main(String[] args) {
 
-        int[] arr = {,3,4,2,2};
+        int[] arr = {1,3,4,2,2};
 
         System.out.println(findDuplicate(arr));
 
