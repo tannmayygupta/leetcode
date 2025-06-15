c5dc5fd6d4bfd1861bfc4c1f631c73061538d800
tannmayygupta <guptatb_1@rknec.edu>
2025-06-15 23:01:12 +0530

    557. Reverse Words in a String III

---
+++ ReverseWoardsinaStringIII.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ReverseWordsinaStringIII{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextInt();
+
+        System.out.println(s);
+    }
+
+    public static String reverseWords(String s) {
+        String[] s2 = s.split("\\s+");
+        StringBuilder s1 = new StringBuilder();
+
+        for (int i = 0; i < s2.length; i++) {
+            StringBuilder word = new StringBuilder(s2[i]);
+            s1.append(word.reverse());
+
+            if (i != s2.length - 1) {
+                s1.append(" ");
+            }
+        }
+
+        return s1.toString();
+    }
+}
