fa3a020b8dd9792f9c9eaf934d6c83ab6ea0aaf9
tannmayygupta <guptatb_1@rknec.edu>
2025-06-19 23:49:01 +0530

    Faulty Keyboard(easyy just thought that what question says no rocket science)

---
+++ FaultyKeyboard.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FaultyKeyboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(finalString(s));
+    
+    }
+    public static String finalString(String s) {
+
+        StringBuilder ans = new StringBuilder("");
+
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch == 'i'){
+                ans.reverse();        
+            }
+            else{
+                ans.append(ch);
+            }
+        } 
+
+
+        return ans.toString();  
+        
+    }
+}
