a417b315fd0d9426c41f00318a5a501dbdf4e14b
tannmayygupta <guptatb_1@rknec.edu>
2025-06-26 21:08:50 +0530

    3. Longest Substring Without Repeating Characters(created a frequency array of 128 char so that lowercase and upppercase and space commas and other char can be matchs and calculated the longest substring of repeated char)

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
