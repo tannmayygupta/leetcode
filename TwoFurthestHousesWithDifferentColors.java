22aa52f1145185f5733125a9e8a8d5299bad3e92
tannmayygupta <guptatb_1@rknec.edu>
2025-06-27 14:28:31 +0530

    2078. Two Furthest Houses With Different Colors

---
+++ TwoFurthestHousesWithDifferentColors.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TwoFurthestHousesWithDifferentColors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] colors = {1,1,1,6,1,1,1};
+
+        System.out.println(maxDistance(colors));
+    }
+    public static int maxDistance(int[] colors) {
+
+        int max = 0 ;
+        
+        for(int i = 0 ; i < colors.length ; i++){
+            for(int j = i + 1 ; j < colors.length ; j++){
+                if(colors[i] != colors[j]){
+                    max = Math.max(max,Math.abs(i - j));
+                }
+            }
+        }
+
+        return max ;
+        
+    }
+}
