c3945cebaeda9c1ff6b81f0cdc30fdc1061430ee
tannmayygupta <guptatb_1@rknec.edu>
2025-06-29 22:35:08 +0530

    1790. Check if One String Swap Can Make Strings Equal

--- CheckifOneStringSwapCanMakeStringsEqual.java
+++ CheckifOneStringSwapCanMakeStringsEqual.java
@@ -1,9 +1,22 @@
 public class CheckifOneStringSwapCanMakeStringsEqual {
-    int count = 0 ;
+
+    public static void main(String[] args) {
+
+        String s1 = "leetcode";
+
+        String s2 = "leetcode";
+
+        
+        int count = 0 ;
+        
         int first = -1 ;
+        
         int second = -1 ;
+        
         for(int i = 0 ; i < s1.length() ; i++){
-            if(s1.charAt(i) != s2.charAt(i)){
+            char ch = s1.charAt(i);
+
+            if(ch != s2.charAt(i)){
                 count++ ;
 
                 if(first == -1){
@@ -15,6 +28,9 @@ public class CheckifOneStringSwapCanMakeStringsEqual {
                 }
             }
         }
+
+
+    }
 
         // if(count > 2){
         //     return false ;
