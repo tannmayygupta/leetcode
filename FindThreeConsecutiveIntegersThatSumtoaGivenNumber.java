6ff91bb950b4b862b8d298b101b2b02fb9dfeb9d
tannmayygupta <guptatb_1@rknec.edu>
2025-06-30 16:23:04 +0530

    2177. Find Three Consecutive Integers That Sum to a Given Number(simply done as we calculate n as we have given sum in class 8th by letting x, x+1, x+2)

---
+++ FindThreeConsecutiveIntegersThatSumtoaGivenNumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class FindThreeConsecutiveIntegersThatSumtoaGivenNumber {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextLong();
+
+        System.out.println(sumOfThree(num).toString());
+
+    }
+
+    public static long[] sumOfThree(long num) {
+
+        long[] arr = new long[3];
+
+        long n = num - 3 ;
+
+        if(n % 3 != 0){
+            return new long[0]  ;
+        }
+
+        arr[0] = n / 3 ;
+
+        arr[1] = arr[0] + 1 ;
+
+        arr[2] = arr[0] + 2 ;
+
+        return arr ;
+        
+    }
+
+}
