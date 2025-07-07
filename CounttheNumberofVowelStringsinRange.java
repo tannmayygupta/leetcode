f86ba291d349d35c93224c36034ed6e22e0c90af
tannmayygupta <guptatb_1@rknec.edu>
2025-07-07 23:44:56 +0530

    Intuition: tried to do what question says and tried the bruteforce approach Complexity: Time complexity:O(n), Space complexity:O(1) Approach: for loop in range given left to right both inclusive and iterate in string arr and then by taking 2 fixed pointer at st and end and a function to check whether the char at st and end is vowel or not and increment the cou and return it

---
+++ CounttheNumberofVowelStringsinRange.java
@@ -0,0 +1,27 @@
+public class CounttheNumberofVowelStringsinRange {
+    public static void main(String[] args) {
+        String[] words = {"are","amy","u"};
+        
+        int left = 0 ;
+        
+        int right = 2 ;
+    }
+    public static int vowelStrings(String[] words, int left, int right) {
+        int cou = 0 ;
+        for(int i = left ; i <= right ; i++){
+            String s1 = words[i];
+            int st = 0 ;
+            int end = s1.length() - 1 ;
+
+            if(isvowel(s1.charAt(st)) && isvowel(s1.charAt(end))){
+                cou++;
+            }
+        }
+
+        return cou ;
+    }
+
+    public static boolean isvowel(char ch){
+        return ch == 'a' || ch == 'u' || ch == 'o' ||ch == 'i' ||ch == 'e' ;
+    }
+}
