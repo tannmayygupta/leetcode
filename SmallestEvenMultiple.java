24391f15fe0f4480f2cef8845f9c6dca68244b74
tannmayygupta <guptatb_1@rknec.edu>
2025-07-07 23:32:18 +0530

    Intuition easypizy
    Approach
    if num is even then we have its return num otherwise return n * 2 >
    
    Complexity
    Time complexity:O(n)
    Space complexity:O(1)

---
+++ SmallestEvenMultiple.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SmallestEvenMultiple {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(smallestEvenMultiple(n));
+    }
+
+    public static int smallestEvenMultiple(int n) {
+
+        if(n % 2 == 0){
+            return n ;
+        }
+        else{
+            return n *  2 ;
+        }
+        
+    }
+}
