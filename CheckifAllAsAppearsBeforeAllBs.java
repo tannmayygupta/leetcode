4405ec997acdda0aa4f2d7252fe119db7f5998ce
tannmayygupta <guptatb_1@rknec.edu>
2025-07-09 22:58:20 +0530

    2124. Check if All A's Appears Before All B's(Intuition: bruteforce Approach: iterate throught the string if count of b is > than 0 and then a appears return false otherwise return true ; Complexity: Time complexity: O(n) Space complexity: O(1))

--- CheckifAllAsAppearsBeforeAllBs.java
+++ CheckifAllAsAppearsBeforeAllBs.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class CheckifAllAsAppearsBeforeAllBs {
+
+    // 2124. Check if All A's Appears Before All B's
+
+    // https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/description/
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -8,7 +12,7 @@ public class CheckifAllAsAppearsBeforeAllBs {
         String s = sc.nextLine();
 
         System.out.println(checkString(s));
-        
+
     }
 
     public static boolean checkString(String s) {
