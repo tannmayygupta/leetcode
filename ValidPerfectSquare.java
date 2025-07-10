db34bf08209f9aa4850a4a5f71315e9cdbb0cd6e
tannmayygupta <guptatb_1@rknec.edu>
2025-07-10 17:57:30 +0530

    Intuition: bruteforce Approach: // 1) binary search // 2) find pattern in square of num that rem they give when the// divide it by 10 Complexity: Time complexity: O(n),Space complexity: O(1)

---
+++ ValidPerfectSquare.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ValidPerfectSquare {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(isPerfectSquare(num));
+        
+    }
+    // 367. Valid Perfect Square
+
+    public static boolean isPerfectSquare(int num) {
+
+        // prefect square ka jab bhi baat hoo 
+        // 1) binary search 
+        // 2) find pattern in square of num that rem they give when the divide it by 10
+
+        for(long i = 1 ; i * i <= num ; i++){
+
+            long in = i*i ;
+            if( in == num){
+                return true ;
+            }
+        }
+
+        return false ;
+    }
+}
