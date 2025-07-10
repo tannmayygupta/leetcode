db8d0b7d2368dc4824576ce9478ba93b3a02a794
tannmayygupta <guptatb_1@rknec.edu>
2025-07-10 17:54:01 +0530

    Intuition: bruteforce Approach: only tried to implement what ques says Complexity: Time complexity: O(n^2) Space complexity:O(1)

---
+++ HexadecimalandHexatrigesimalConversion.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class HexadecimalandHexatrigesimalConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(concatHex36(n));
+        
+    }
+    public static String concatHex36(int n) {
+
+        int hexadec = n * n ;
+
+        int hexatri = n * n * n ;
+
+        StringBuilder sb1 = new StringBuilder();
+
+        StringBuilder sb2 = new StringBuilder();
+
+        while(hexadec > 0 ){
+
+            int h = hexadec % 16 ;
+
+            if(h >= 10){
+                sb1.append((char) ('A' + h - 10 ));
+            }
+            else{
+                sb1.append(h);
+            }
+
+            hexadec = hexadec / 16 ;
+        }
+
+        sb1.reverse();
+
+        String s1 = sb1.toString();
+
+        while(hexatri > 0 ){
+
+            int h = hexatri % 36 ;
+
+            if(h >= 10){
+                sb2.append((char) ('A' + h - 10));
+            }
+            else{
+                sb2.append(h);
+            }
+
+            hexatri = hexatri / 36 ;
+        }
+
+        sb2.reverse();
+
+        String s2 = sb2.toString();
+
+        return s1 +  s2 ;
+    }
+}
