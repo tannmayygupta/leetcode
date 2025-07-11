daed68576d78dcc049d29da597f8eb19baf89849
tannmayygupta <guptatb_1@rknec.edu>
2025-07-11 19:14:26 +0530

    1137. N-th Tribonacci Number(2 approach 1) iteration and 2) recurrsive )

--- CountPrimes.java
+++ CountPrimes.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CountPrimes {
@@ -10,29 +11,69 @@ public class CountPrimes {
         System.out.println(countPrimes(n));
 
     }
-    public static int countPrimes(int n) {
-        int mam = 0 ;
-        int n1 = 0 ;
-        while(n1 < n){
-            if(isPrime(n1)){
-                mam++;
-            }
-            n1++;
-        }
-        return mam ;
-    }
 
-    public static boolean isPrime(int n1){
-        if(n1 < 2){
-            return false ;
+    // BRUTEFORCE GETTING TLE NOT OPTIMISED SOLUTION 
+
+    // public static int countPrimes(int n) {
+    //     int mam = 0 ;
+    //     int n1 = 0 ;
+    //     while(n1 < n){
+    //         if(isPrime(n1)){
+    //             mam++;
+    //         }
+    //         n1++;
+    //     }
+    //     return mam ;
+    // }
+
+    // public static boolean isPrime(int n1){
+    //     if(n1 < 2){
+    //         return false ;
+    //     }
+    //     else{
+    //         for(int i = 2 ; i <= Math.sqrt(n1) ; i++){
+    //             if(n1 % i == 0){
+    //                 return false ;
+    //             }
+    //         }
+    //         return true ;
+    //     }
+    // }
+
+
+    // HERE IS THE SIEVE METHOD WHAT IT DO IS I HAVE LEARBED IT 
+    // we know 2 is prime then multiple of 2 in range n will not be prime 
+    // same goes with 3, 5, 7
+
+    public static int countPrimes(int n) {
+
+        if(n <= 2){
+            return 0 ;
         }
-        else{
-            for(int i = 2 ; i <= Math.sqrt(n1) ; i++){
-                if(n1 % i == 0){
-                    return false ;
+
+        boolean[] arr = new boolean[n];
+
+        Arrays.fill(arr,true);
+
+        arr[0] = arr[1] == false ;
+
+        for(int i = 2 ; i <= (int) Math.sqrt(n) ; i++){
+            if(arr[i]){
+                for(int j = i * i ; j < n ; j+=i){
+                    arr[j] = false ;
                 }
             }
-            return true ;
         }
+
+        int cou = 0 ;
+
+        for(int i = 2 ; i < arr.length ; i++){
+            if(arr[i]){
+                ++cou;
+            }
+        }
+
+        return cou ;
+
     }
 }
---
+++ NthTribonacciNumber.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class NthTribonacciNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(tribonacci(n));
+
+    }
+    public static int tribonacci(int n) {
+        
+        // m1 iterative 
+
+        int t0  = 0 ; 
+
+        int tn  = 0 ;
+
+        int t1  = 1 ; 
+
+        int t2  = 1 ;
+
+        if(n == 0){
+            return 0 ;
+        }
+        else if(n == 1 || n == 2){
+            return 1 ;
+        } 
+        else{
+            while(n > 2){
+                tn = t0 + t1 + t2 ;
+                t0 = t1 ;
+                t1 = t2 ;
+                t2 = tn ;
+                n--;
+
+            }
+        }
+
+        return tn ;
+
+        // m2: recursion (but getting tle in recursion)
+
+        // base conditions 
+        // if(n == 0){
+        //     return 0 ;
+        // }
+        // else if(n == 1 || n == 2){
+        //     return 1 ;
+        // } 
+
+        // // recursive calls
+        // return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1) ;
+        
+    }
+}
