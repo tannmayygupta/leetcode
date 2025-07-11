4c4640f847af51ac849d939712905a79ac15cef1
tannmayygupta <guptatb_1@rknec.edu>
2025-07-11 19:34:57 +0530

    1018. Binary Prefix Divisible By 5(bruteforce)

---
+++ BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryPrefixDivisibleBy5 {
+    public static void main(String[] args) {
+        int[] nums = {1,0,1,0,1,0,1,0,1};
+
+        System.out.println(prefixesDivBy5(nums));
+
+    }
+    public static List<Boolean> prefixesDivBy5(int[] nums) {
+
+        List<Boolean> ans = new ArrayList<>();
+
+        int curr = 0 ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            curr = (curr * 2 + nums[i]) % 5 ;
+            ans.add(curr == 0);
+        }
+
+        return ans ;
+    }
+}
