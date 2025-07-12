b1e5a6d72b77184cec11f3f1792059f7b00d7c79
tannmayygupta <guptatb_1@rknec.edu>
2025-07-12 21:30:11 +0530

    217. Contains Duplicate(Approach: used hashmap for tracking duplicate element in the array nums if(map.contains(nums[i])){ return true else put the nums in map})

---
+++ ContainsDuplicate.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+
+        System.out.println(containsDuplicate(nums));
+        
+    }
+    public static boolean containsDuplicate(int[] nums) {
+       
+    HashMap<Integer ,Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(map.containsKey(nums[i])){
+                return true ;
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+        
+    }
+}
