6f47a345d5a225c7bd68578ab26c3c723ab66735
tannmayygupta <guptatb_1@rknec.edu>
2025-07-12 21:53:20 +0530

    217. Contains Duplicate II(Approach: used hashmap easy to understand bruteforce)

--- ContainsDuplicateII.java
+++ ContainsDuplicateII.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 
 public class ContainsDuplicateII {
+
+    // 219. Contains Duplicate II
     public static void main(String[] args) {
         int[] nums = {1,2,3,1,2,3};
         int k = 2 ;
