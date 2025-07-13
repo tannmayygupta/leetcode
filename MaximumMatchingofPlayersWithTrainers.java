2f31ee696d9379b989359be0d9bfe6d9b38ce741
tannmayygupta <guptatb_1@rknec.edu>
2025-07-13 15:37:43 +0530

    2410. Maximum Matching of Players With Trainers(two-pointer easily to understand just sorted the given arrays and then intialise p1 and t1 and move accordingly)

---
+++ MaximumMatchingofPlayersWithTrainers.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class MaximumMatchingofPlayersWithTrainers {
+    public static void main(String[] args) {
+
+        int[] player = {4,7,9};
+
+        int[] trainers = {8,2,5,8};
+
+        System.out.println(matchPlayersAndTrainers(player, trainers));
+        
+    }
+    public static int matchPlayersAndTrainers(int[] players, int[] trainers) {
+
+        Arrays.sort(players);
+
+        Arrays.sort(trainers);
+
+        int p1 = 0 ;
+        int t1 = 0 ;
+
+        int cou = 0 ;
+        while(p1 < players.length && t1 < trainers.length){
+            if(players[p1] <= trainers[t1]){
+                cou++;
+                p1++;
+                t1++;
+            }
+            else{
+                t1++;
+            }
+        }
+
+        return cou ;
+        
+    }
+}
