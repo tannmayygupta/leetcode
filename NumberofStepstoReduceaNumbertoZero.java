61a63a321eecd3db3e102ee2282eafa0d7ba63db
tannmayygupta <guptatb_1@rknec.edu>
2025-07-18 22:51:55 +0530

    1342. Number of Steps to Reduce a Number to Zero(recursive approach)

---
+++ NumberofStepstoReduceaNumbertoZero.java
@@ -0,0 +1,24 @@
+public class NumberofStepstoReduceaNumbertoZero {
+    public static void main(String[] args) {
+
+        int num = 14 ;
+
+        System.out.println(numberOfSteps(num));
+        
+    }
+    public static int numberOfSteps(int num) {
+        return helper(num , 0);
+    }
+
+    static int helper(int num , int step){
+        if(num == 0){
+            return step ;
+        }
+
+        if(num % 2 == 0){
+            return helper(num/2 , step+1);
+        }
+
+        return helper(num - 1 , step + 1);
+    }
+}
