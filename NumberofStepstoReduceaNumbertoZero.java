6ce66b64a664e04c2f733d6ff8b2268c82918cab
tannmayygupta <guptatb_1@rknec.edu>
2025-07-18 22:52:36 +0530

    1342. Number of Steps to Reduce a Number to Zero(recursive approach)

--- NumberofStepstoReduceaNumbertoZero.java
+++ NumberofStepstoReduceaNumbertoZero.java
@@ -1,4 +1,6 @@
 public class NumberofStepstoReduceaNumbertoZero {
+    // 1342. Number of Steps to Reduce a Number to Zero
+    // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
     public static void main(String[] args) {
 
         int num = 14 ;
