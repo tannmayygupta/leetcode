58b5c776defb57d173685a0e40222f23e9a5be95
tannmayygupta <guptatb_1@rknec.edu>
2025-07-18 22:53:47 +0530

    412. Fizz Buzz(iterative approach)

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
-
+// 412. Fizz Buzz
+// https://leetcode.com/problems/fizz-buzz/
 public class FizzBuzz {
     public static void main(String[] args) {
 
