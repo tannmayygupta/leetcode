d4528d7b767cc2f8242db1710522f41c6ec6be59
tannmayygupta <guptatb_1@rknec.edu>
2025-07-22 19:21:28 +0530

    3618. Split Array by Prime Indices(done with sieves method easyy just wass* getting error due to type case to long

--- SplitArraybyPrimeIndices.java
+++ SplitArraybyPrimeIndices.java
@@ -10,84 +10,85 @@ public class SplitArraybyPrimeIndices {
     }
     public static long splitArray(int[] nums) {
 
-        int n = nums.length ; 
+    //     int n = nums.length ; 
+        
+    //     if(n == 1 ){
+    //         return (long) Math.abs(nums[0]);
+    //     }
+
+    //     long sum2 = nums[0] + nums[1] ;
+
+    //     if(n == 2){
+    //         return (long) Math.abs(sum2) ;
+    //     }
+
+    //     boolean[] isprime = new boolean[n];
+
+    //     Arrays.fill(isprime,true);
+
+    //     isprime[0]= false ;
+        
+    //     isprime[1]= false ;
+
+    //     for(int i = 2 ; i <= Math.ceil(Math.sqrt(n)) ; i++){
+    //         if(isprime[i]){
+    //             for(int j = i * i ; j < n ; j+=i){
+    //                 isprime[j] = false ;
+    //             }
+    //         }
+    //     }
+
+    //     long sumA = 0 ;
+
+    //     long sumB = 0 ;
+
+    //     for(int i = 0 ; i < n ; i++){
+    //         if(isprime[i]){
+    //             sumA+=nums[i];
+    //         }
+    //         else{
+    //             sumB+=nums[i]; 
+    //         }
+    //     }
+
+    //     return (long) Math.abs(sumA - sumB);
+
+        // sieves thoda complicate kar diya mai 
+
+        int n = nums.length ;
+        
+        long sum2 = nums[0] + nums[1] ;
         
         if(n == 1 ){
             return (long) Math.abs(nums[0]);
         }
-
-        long sum2 = nums[0] + nums[1] ;
-
         if(n == 2){
             return (long) Math.abs(sum2) ;
         }
 
-        boolean[] isprime = new boolean[n];
+        boolean[] arr = new boolean[n];
 
-        Arrays.fill(isprime,true);
-
-        isprime[0]= false ;
-        
-        isprime[1]= false ;
-
-        for(int i = 2 ; i <= Math.ceil(Math.sqrt(n)) ; i++){
-            if(isprime[i]){
-                for(int j = i * i ; j < n ; j+=i){
-                    isprime[j] = false ;
-                }
-            }
-        }
+        Arrays.fill(arr,true);
 
         long sumA = 0 ;
 
         long sumB = 0 ;
 
-        for(int i = 0 ; i < n ; i++){
-            if(isprime[i]){
-                sumA+=nums[i];
-            }
-            else{
-                sumB+=nums[i]; 
+        for(int i = 2 ; i < n ; i++){
+            if(arr[i]){
+                sumA += nums[i] ;
+                for(long j =(long) i * i ; j < n ; j+=i){
+                    int ind = (int) j;
+                    sumB += (long) nums[ind] ;
+                    nums[ind] = 0 ;
+                    arr[ind] = false ;
+                }
             }
         }
 
+        sumB = sumB + sum2 ;
+
         return (long) Math.abs(sumA - sumB);
-
-        // sieves thoda complicate kar diya mai 
-
-        // int n = nums.length ;
-        
-        // long sum2 = nums[0] + nums[1] ;
-        
-        // if(n == 1 ){
-        //     return (long) Math.abs(nums[0]);
-        // }
-        // if(n == 2){
-        //     return (long) Math.abs(sum2) ;
-        // }
-
-        // boolean[] arr = new boolean[n];
-
-        // Arrays.fill(arr,true);
-
-        // long sumA = 0 ;
-
-        // long sumB = 0 ;
-
-        // for(int i = 2 ; i < n || i <= (int) Math.ceil(Math.sqrt(n)) ; i++){
-        //     if(arr[i]){
-        //         sumA += nums[i] ;
-        //         for(int j = i * i ; j < n ; j+=i){
-        //             sumB +=nums[j] ;
-        //             nums[j] = 0 ;
-        //             arr[j] = false ;
-        //         }
-        //     }
-        // }
-
-        // sumB = sumB + sum2 ;
-
-        // return (long) Math.abs(sumA - sumB);
         
     }
 }
