48a136e0064d957487ab2f74a467857c2de03542
tannmayygupta <guptatb_1@rknec.edu>
2025-07-25 20:58:40 +0530

    2053. Kth Distinct String in an Array(used hashmap see the solution that you posted on leetcode section to get the context)

--- KthDistinctStringinanArray.java
+++ KthDistinctStringinanArray.java
@@ -11,6 +11,7 @@ public class KthDistinctStringinanArray {
     public static String kthDistinct(String[] arr, int k) {
         HashMap<String,Integer> ans = new HashMap<>();
 
+        
         for(int i = 0 ; i < arr.length ; i++){
             if(ans.containsKey(arr[i])){
                 int cur = ans.get(arr[i]);
