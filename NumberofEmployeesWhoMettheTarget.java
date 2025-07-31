211890db6c067b95ead004f7241a89ba1460802b
tannmayygupta <tanmayg460@gmail.com>
2025-07-31 19:54:04 +0530

    2798. Number of Employees Who Met the Target

---
+++ NumberofEmployeesWhoMettheTarget.java
@@ -0,0 +1,19 @@
+public class NumberofEmployeesWhoMettheTarget {
+    public static void main(String[] args) {
+        int[] hours = {0,1,2,3,4};
+        int target = 2;
+        System.out.println(numberOfEmployeesWhoMetTarget(hours, target));
+    }
+    public static int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+
+        int cou = 0 ;
+
+        for(int i = 0 ; i < hours.length ; i++){
+            if(hours[i] >= target){
+                cou++;
+            }
+        }
+        
+        return cou ;
+    }
+}
