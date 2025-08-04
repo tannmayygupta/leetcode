9cf451e431f2ab68b7e345128379d0ac4f7ba56f
tannmayygupta <tanmayg460@gmail.com>
2025-08-04 23:11:01 +0530

    137. Single Number II(iterative approach basic if want to revice go and see leetcode solution that you uploaded)

---
+++ SingleNumberII.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class SingleNumberII {
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,2};
+
+        System.out.println(singleNumber(arr));
+    
+    }
+    public static int singleNumber(int[] arr) {
+
+        if(arr.length == 1){
+            return arr[0];
+        }
+        
+        Arrays.sort(arr);
+
+        int i = 0 ; 
+
+        while(i < arr.length){
+            if((i < arr.length - 2) && (arr[i] == arr[i+1]) && (arr[i+1] == arr[i+2])){
+                i=i+3;
+            }
+            else if(i > 0 && i < arr.length - 1 && arr[i-1] != arr[i] && arr[i] != arr[i+1] ){
+                return arr[i];
+            }
+            else if(i == 0 && arr[i] != arr[i+1]){
+                return arr[i];
+            }
+            else if(i == arr.length - 1 && arr[i] != arr[i-1]){
+                return arr[i];
+            }else{
+                i++;
+            }
+        }
+        return -1 ;
+    }
+}
