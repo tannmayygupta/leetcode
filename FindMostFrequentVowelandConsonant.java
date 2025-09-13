095785e121b18d2f5c9a61a09e0973a858ac217a
tannmayygupta <tanmayg460@gmail.com>
2025-09-13 19:18:36 +0530

    3541. Find Most Frequent Vowel and Consonant(easy implementation for the max freq of vowel and consonants)

---
+++ FindMostFrequentVowelandConsonant.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class FindMostFrequentVowelandConsonant {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+
+        System.out.println(maxFreqSum(s));
+    }
+
+    static int maxFreqSum(String s) {
+        int[] fre = new int[26];
+
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            int c = ch - 'a' ;
+            fre[c]++;
+        }
+
+        int max_vowel = 0 ;
+        int max_consonant = 0 ;
+
+        int a = 'a' - 'a';
+        int i = 'i' - 'a';
+        int o = 'o' - 'a';
+        int e = 'e' - 'a';
+        int u = 'u' - 'a';
+
+        for(int j = 0 ; j < 26 ; j++){
+            if(j == a || j == e || j == i || j == o || j == u ){
+                if(max_vowel < fre[j]){
+                    max_vowel = fre[j] ;
+                }
+            }else{
+                if(max_consonant < fre[j]) {
+                    max_consonant=fre[j];
+                }
+            }
+        }
+
+        return max_consonant + max_vowel ;
+
+    }
+}
