330c5a78fd6e3e6384cc690b49ee56543b421ec5
tannmayygupta <tanmayg460@gmail.com>
2025-09-21 00:26:33 +0530

    3000. Maximum Area of Longest Diagonal Rectangle(easy solution bruteforce only this is to see the condition for daigonal wen its equal and area is more than the last diagonal of equal length)

---
+++ MaximumAreaofLongestDiagonalRectangle.java
@@ -0,0 +1,34 @@
+public class MaximumAreaofLongestDiagonalRectangle {
+    public static void main(String[] args) {
+        int[][] dimensions = {{6,5},{8,6},{2,10},{8,1},{9,2},{3,5},{3,5}};
+
+        int a = areaOfMaxDiagonal(dimensions);
+        
+        System.out.println(a);
+    }
+    public static int areaOfMaxDiagonal(int[][] dimensions) {
+
+        double max = 0 ;
+        int maxarea = 0 ;
+
+        for(int i = 0 ; i < dimensions.length ; i++){
+
+            double length1 =  (double) Math.sqrt( (double) (dimensions[i][0] * dimensions[i][0] + dimensions[i][1] * dimensions[i][1]) );
+
+            if(length1 >= max){
+                max = length1 ;
+
+                int area = dimensions[i][0] * dimensions[i][1] ;
+
+                if(maxarea <= area){
+                    maxarea = area ;
+                }
+
+            }
+
+            
+        }
+        
+        return maxarea ; 
+    }
+}
