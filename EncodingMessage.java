8e9e92608333095fa02d521d48bba323576080d7
tannmayygupta <tanmayg460@gmail.com>
2025-12-29 00:00:39 +0530

    Encoding Message

---
+++ EncodingMessage.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class EncodingMessage {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    
+		    String s = sc.next();
+		    
+		    char[] ch = s.toCharArray();
+		    
+		    for(int i = 0 ; i < ch.length - 1 ; i+=2){
+		        char temp = ch[i];
+		        ch[i] = ch[i+1];
+		        ch[i+1] = temp ;
+		    }
+		    
+		    for(int i = 0 ; i < ch.length ; i++){
+		        int c = ch[i] - 'a' ;
+		        char ch1 = (char) ('z' - c) ;
+		        ch[i] = ch1 ;
+		    }
+		    
+		    System.out.println(String.valueOf(ch));
+		    
+		}
+
+	}
+}
