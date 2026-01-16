32a448c0f04bf790ea65713a484ad3f289425ff1
tannmayygupta <tanmayg460@gmail.com>
2026-01-16 20:37:49 +0530

    713. Subarray Product Less Than K(lc using sliding window)

---
+++ SubarrayProductLessThanK.java
@@ -0,0 +1,29 @@
+public class SubarrayProductLessThanK {
+    public static void main(String[] args) {
+        int[] nums = {10,5,2,6};
+        int k = 100;
+    }
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int product = 1 ;
+        int j = 0 ;
+        int ans = 0 ; 
+
+        if(k <= 1){
+            return 0 ; 
+        }
+
+        // this expand the window till the condition of window get true
+        for(int i = 0 ; i < nums.length ; i++){
+            product = product * nums[i];
+
+            // if we get the window holding thw condtion we shrik the window
+            while(product >= k){
+                product = product / nums[j] ;
+                j++;
+            }
+            ans = ans + i - j + 1 ;
+        }
+
+        return ans ;
+    }
+}
