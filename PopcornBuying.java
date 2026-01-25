6a932a1624da4decfa0ee4e46b2eb6803b6c2370
tannmayygupta <tanmayg460@gmail.com>
2026-01-25 23:00:25 +0530

    // Popcorn Buying

---
+++ PopcornBuying.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PopcornBuying {
+    public static void main(String[] args) {
+        // Popcorn Buying
+        Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		System.out.println( (x - 100) / 50 );
+    }
+}
