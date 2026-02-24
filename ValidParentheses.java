3007b7a5ca50837f353d702a91f17d83ed0c4b59
tannmayygupta <tanmayg460@gmail.com>
2026-02-24 22:52:02 +0530

    445. Add Two Numbers II(simple stack we push ')' this is for opening '(' and similar for ther brackets)

---
+++ ValidParentheses.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "()]]]]]]";
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s) {
+        
+        Stack<Character> stk = new Stack<>();
+
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '(' ){
+                stk.push(')');
+            }
+            else if(ch == '[' ){
+                stk.push(']');
+            }
+            else if(ch == '{') {
+                stk.push('}');
+            }
+            else if(!stk.empty() && stk.peek() == ch ){ // if we dont check for empty
+                //tc like s = ']' ;
+                // as no opening bracket we will not able to push this and not able to check for peek
+                stk.pop();
+            }
+            else if(!stk.empty() && stk.peek() == ch ){
+                stk.pop();   
+            }
+            else if(!stk.empty() && stk.peek() == ch ){
+                stk.pop();
+            }else{
+                return false ;
+            }
+
+        }
+
+        return stk.empty() ;
+        
+    }
+}
