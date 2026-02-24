e22c3754a899ca87366d9872ab21e0afc198de76
tannmayygupta <tanmayg460@gmail.com>
2026-02-24 22:52:27 +0530

    445. Add Two Numbers II(simple stack we push ')' this is for opening '(' and similar for ther brackets)

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,5 +1,6 @@
 import java.util.Stack;
-
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/
 public class ValidParentheses {
     public static void main(String[] args) {
         String s = "()]]]]]]";
