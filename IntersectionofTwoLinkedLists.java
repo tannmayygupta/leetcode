00bfb987048351052511eaa9c0f397e1c6157ffe
tannmayygupta <tanmayg460@gmail.com>
2026-02-28 16:16:56 +0530

    160. Intersection of Two Linked Lists(easy 2 poiinter)

---
+++ IntersectionofTwoLinkedLists.java
@@ -0,0 +1,45 @@
+public class IntersectionofTwoLinkedLists {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode temp1 = headA ;
+        ListNode temp2 = headB ;
+
+
+        while(temp1 != temp2){
+            if(temp1 == null){
+                temp1 = headB ;
+            }else{
+                temp1 = temp1.next ;
+            }
+
+            if(temp2 == null){
+                temp2 = headA ; // when temp null the head shifting id one operation 
+            }
+            else{
+                temp2 = temp2.next ; // this if not null is operation 
+            }
+        }
+
+        return temp1 ;
+        
+    }
+}
