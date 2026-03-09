c70fd8c2ae0abf477882f1106cb5d49c8021da01
tannmayygupta <tanmayg460@gmail.com>
2026-03-09 23:38:22 +0530

    tried area of histogram didnt get the logic

---
+++ LargestRectangleinHistogram.java
@@ -0,0 +1,11 @@
+public class LargestRectangleinHistogram {
+    public static void main(String[] args) {
+        int[] heigth = {};
+        System.out.println(largestRectangleArea(heigth));
+    }
+    public static int largestRectangleArea(int[] heights) {
+        int maxArea = Integer.MIN_VALUE ;
+        
+        return maxArea ;
+    }
+}
