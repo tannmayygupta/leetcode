f051148ca45f7fbd9339c4372ca72b8519739d9e
tannmayygupta <tanmayg460@gmail.com>
2026-03-10 21:09:29 +0530

    Shortest Job first (GFG greedy)

---
+++ ShortestJobfirst.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ShortestJobfirst {
+    public static void main(String[] args) {
+        int[] bt = {4,3,7,1,2};
+        System.out.println(solve(bt));
+    }
+    static int solve(int bt[]) {
+        // code here
+        Arrays.sort(bt);
+        
+        int time = 0 ;
+        int ans = 0 ;
+        //  1 , 2 , 3 , 4 , 7 
+        for(int i = 0 ; i < bt.length ; i++){
+            ans+=time ;
+            time += bt[i];
+        }
+        
+        return (int) (ans / bt.length) ;
+    }
+}
